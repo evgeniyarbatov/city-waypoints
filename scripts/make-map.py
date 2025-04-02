e01ab68f68fc20835150c54dd473507e1fbb7ebf
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-04-02 20:52:23 +0700

    Simplify better with gpsbabel

--- scripts/make-map.py
+++ scripts/make-map.py
@@ -60,15 +60,6 @@ class PointGraph:
         ET.indent(tree, space="  ")
         tree.write(filename, encoding="utf-8", xml_declaration=True)
 
-def simplify_gpx(map_filename):
-    with open(map_filename, 'r') as f:
-        gpx = gpxpy.parse(f)
-
-    gpx.simplify()
-
-    with open(map_filename, 'w') as f:
-        f.write(gpx.to_xml())
-
 def main(
     routes_filename,
     map_filename,
@@ -88,8 +79,6 @@ def main(
     path = pg.dfs_with_backtracking(start_point)
      
     pg.save_to_gpx(path, map_filename)
-    
-    # simplify_gpx(map_filename)
   
 if __name__ == "__main__":
     main(*sys.argv[1:])
